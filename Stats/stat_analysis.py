20379ca9edcc638034f5d031ca9c2feb3a6677c7
nday0006 <perle-marie-salma.ndayizeye@etudiant.univ-reims.fr>
2025-02-21 20:38:28 +0100

    Descriptive statistics on datasets

--- Stats/stat_analysis.py
+++ Stats/stat_analysis.py
@@ -102,22 +102,48 @@ print(dishes.columns)
 print(dishes["product_allergen"].unique())
 print(dishes['product_nutrient'].value_counts())
 
-dish_counts = dishes['dish_name'].value_counts()
-
-# barplot
-sns.barplot(x=dish_counts.index, y=dish_counts.values)
-plt.title('Dish frequency')
-plt.xlabel('Dish name')
-plt.ylabel('Number of occurrences')
-plt.show()
 
 allergen_dish_counts = dishes.groupby('product_allergen')['dish_name'].nunique()
+print(allergen_dish_counts)
+dish_counts = dishes['dish_name'].value_counts()
+
+# Barplot 
+sns.set_style("whitegrid")
+plt.figure(figsize=(12, 6))
 
 
+sns.barplot(x=dish_counts.index, y=dish_counts.values, palette="viridis")
+
+
+plt.title('Dish Frequency', fontsize=14)
+plt.xlabel('Dish Name', fontsize=12)
+plt.ylabel('Number of Occurrences', fontsize=12)
+
+plt.xticks(rotation=30, ha='right', fontsize=10)
+
+plt.tight_layout()
+plt.show()
+
+
+import textwrap
+plt.figure(figsize=(14, 6))
 sns.barplot(x=allergen_dish_counts.index, y=allergen_dish_counts.values)
 plt.title('Number of dishes per allergen')
 plt.xlabel('Allergen')
 plt.ylabel('Number of unique dishes')
+#labels = [textwrap.fill(label, width=10) for label in allergen_dish_counts.index]
+#plt.xticks(range(len(labels)), labels, rotation=45, ha='right', fontsize=8)
+plt.xticks(rotation=45, ha='right',fontsize=8) 
+#plt.tight_layout()
 plt.show()
-#correlation_matrix = dishes.corr()
-#print(correlation_matrix)
+
+###Cross tables 
+pd.crosstab(dishes["dish_name"], dishes["product_allergen"])
+pd.crosstab(menu["meal_type"], menu["dish_type"])
+
+
+menu.groupby("menurestaurant_type")["dish_name"].count()
+menu.groupby("menurestaurant_type")["dish_type"].nunique()
+menu.groupby("meal_type")["dish_type"].value_counts()
+
+print(menu["menuPlatNom"].value_counts(normalize=True) * 100)
