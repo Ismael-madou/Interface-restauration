5321d8db7e2ad7cbd5ce93b364cd3cac29452e59
nday0006 <perle-marie-salma.ndayizeye@etudiant.univ-reims.fr>
2025-02-23 02:03:25 +0100

    Mega modif

--- scripts/cleaning_processing.py
+++ scripts/cleaning_processing.py
@@ -8,8 +8,8 @@ import os
 # Défine file path
 BASE_DIR = Path(os.getcwd())  
 print(BASE_DIR)
-DATABASE_DIR = BASE_DIR / "Databases"  # excel file repository
-
+DATABASE_DIR = BASE_DIR / "data/raw"  # excel file repository
+PROCESSED_DIR = Path("data/processed")  # processed file repository
 
 menu = pd.read_excel(DATABASE_DIR / "antibes-menu-2025.xlsx")
 dishes = pd.read_excel(DATABASE_DIR / "antibes-plats-2025.xlsx")
@@ -97,53 +97,6 @@ new_names = {
 dishes.rename(columns=new_names, inplace=True)
 print(dishes.columns)
 
-# ##DESCRIPTIVE STATISTICS 
- 
-print(dishes["product_allergen"].unique())
-print(dishes['product_nutrient'].value_counts())
-
-
-allergen_dish_counts = dishes.groupby('product_allergen')['dish_name'].nunique()
-print(allergen_dish_counts)
-dish_counts = dishes['dish_name'].value_counts()
-
-# Barplot 
-sns.set_style("whitegrid")
-plt.figure(figsize=(12, 6))
-
-
-sns.barplot(x=dish_counts.index, y=dish_counts.values, palette="viridis")
-
-
-plt.title('Dish Frequency', fontsize=14)
-plt.xlabel('Dish Name', fontsize=12)
-plt.ylabel('Number of Occurrences', fontsize=12)
-
-plt.xticks(rotation=30, ha='right', fontsize=10)
-
-plt.tight_layout()
-plt.show()
-
-
-import textwrap
-plt.figure(figsize=(14, 6))
-sns.barplot(x=allergen_dish_counts.index, y=allergen_dish_counts.values)
-plt.title('Number of dishes per allergen')
-plt.xlabel('Allergen')
-plt.ylabel('Number of unique dishes')
-#labels = [textwrap.fill(label, width=10) for label in allergen_dish_counts.index]
-#plt.xticks(range(len(labels)), labels, rotation=45, ha='right', fontsize=8)
-plt.xticks(rotation=45, ha='right',fontsize=8) 
-#plt.tight_layout()
-plt.show()
-
-###Cross tables 
-pd.crosstab(dishes["dish_name"], dishes["product_allergen"])
-pd.crosstab(menu["meal_type"], menu["dish_type"])
-
-
-menu.groupby("menurestaurant_type")["dish_name"].count()
-menu.groupby("menurestaurant_type")["dish_type"].nunique()
-menu.groupby("meal_type")["dish_type"].value_counts()
-
-print(menu["menuPlatNom"].value_counts(normalize=True) * 100)
+##Export cleaned datasets 
+menu.to_csv(PROCESSED_DIR / "menu.csv", index=False)
+dishes.to_csv(PROCESSED_DIR / "dishes.csv", index=False)
