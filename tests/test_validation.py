845335919066197212208c1695b21998b062c211
delp0011 <dimitri.delpech@etudiant.univ-reims>
2025-03-26 15:47:46 +0100

    mise a jour des test

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,17 +1,23 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 import unittest
 from unittest.mock import patch
-from validation import validate_snack, validate_product
-from recap import chosen_products
+from src.interface.validation import validate_snack, validate_product
+from src.interface.shared_data import chosen_products
 
 class TestValidationFunctions(unittest.TestCase):
 
     def setUp(self):
         chosen_products.clear()
 
+    @patch('src.interface.recap.print_recap', autospec=True)
     @patch('builtins.input', side_effect=["Sandwich", "no"])
-    def test_validate_snack(self, mock_input):
+    def test_validate_snack(self, mock_input, mock_print_recap):
         products = ["Sandwich", "Chips"]
-        validate_snack(products)
+        with patch('src.interface.validation.print_recap'):
+            validate_snack(products)
         self.assertIn("Sandwich", chosen_products)
 
     @patch('builtins.input', return_value="Pizza")
--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,8 +1,16 @@
-from src.interface.visualization import display_graph_image
+import sys
 from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-graph_image_path = Path("docs/dish_occurrences.png")
+import unittest
+from unittest.mock import patch
+from src.interface.visualization import display_graph_image
 
-print("start test")
-display_graph_image(graph_image_path)
-print("end of test.")
+class TestVisualizationFunctions(unittest.TestCase):
+
+    @patch("builtins.print")
+    def test_display_graph_image_file_not_found(self, mock_print):
+        # Fichier imaginaire pour forcer l'erreur
+        fake_path = Path("docs/non_existing_image.png")
+        display_graph_image(fake_path)
+        mock_print.assert_any_call(f"⚠️ Image not found: {Path(__file__).resolve().parent.parent / fake_path}")
